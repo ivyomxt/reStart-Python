d867176c316ac554e4b5f8ab01f3f9adb70489f6
Vyom <68363141+vyomks5@users.noreply.github.com>
2022-07-01 12:07:19 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,141 @@
+
+# # a = "Python Language"
+# #
+# # print("Myself Vyom")
+# # print("I am using :", a,"to code.")
+# # #########################
+# # a= 5
+# # b = 5
+# # c= a+b
+# # print("sum =", c)
+# # d= a-b
+# # print(d)
+# # e= a*b
+# # print(e)
+# # f= a/b
+# # print(f)
+# #################
+# # n1,n2,n3 = input("enter number:").split()
+# # print("N1=",n1)
+# # print("N2=",n2)
+# # print("N3=",n3)
+# # add = int(n1)+int(n2)+int(n3)
+# # print("N1+N2+N3:",add)
+# # n2= input("Enter n2 : ")
+# # add= int(n1)+int(n2)
+# # print("Addition of n1 + n2 is :",add)
+# # print("Data type of add is:",type(add))
+
+# # statement="My name is Vyom"
+# # print(statement)
+# #############
+# # a= int(input("Enter Number 1:"))
+# # b= int(input("Enter Number 2:"))
+# # c= input("Enter operation +,-,*,/:")
+# # if c=='+':
+# #     print(a+b)
+# # elif c=='-':
+# #     print(a-b)
+# # elif c=='*':
+# #     print(a*b)
+# # elif c=='/':
+# #     print(a/b)
+# # else:
+# #     print("invalid operation")
+# #################
+
+# # a= int(input("Enter Number :"))
+# #
+# # if a>=0:
+# #     print("number is positive")
+# #
+# # else:
+# #     print("Number is negattive")
+# ########################
+
+# # a = int(input("Enter Number :"))
+# # if a%7 == 0:
+# #     print("number is divisible")
+# # else:
+# #     print("not divisible")
+# #################
+
+# # a = int(input("Eneter Number:"))
+# # if a%2==0:
+# #     print("number is even")
+# # else:
+# #     print("number is odd")
+# ##############
+
+# # a = int(input("Entwer Number between 1 to 7: "))
+# # if a==1:
+# #     print("Sunday")
+# # elif a==2:
+# #     print("Monday")
+# # elif a==3:
+# #     print("Tuesday")
+# # elif a==4:
+# #     print("Wednesday")
+# # elif a==5:
+# #     print("Thursday")
+# # elif a==6:
+# #     print("Friday")
+# # elif a==7:
+# #     print("Saturday")
+# # else:
+# #     print("Not a valid input")
+# ######################
+
+# # a=int(input("Enter number"))
+# # if a%2&3==0:
+# #     print("number is divisible")
+# # else:
+# #     print("number is not divisible")
+
+# ##################
+
+# # from turtle import clear
+
+
+# # a=input("enter any alphabets: ")
+# # if a=="a":
+# #     print("It is a vowel.")
+# # elif a=="e":
+# #     print("It is a vowel.")
+# # elif a=="i":
+# #     print("It is a vowel.")
+# # elif a=="o":
+# #     print("It is a vowel.")
+# # elif a=="u":
+# #     print("It is a vowel.")
+# # else:
+# #     print("It is a consonent.")
+# #####
+# # a=input("enter any alphabets: ")
+# # if a=="aeiouAEIOU":
+# #     print("It is a vowel.")
+# # else:
+# #     print("not a vowel")
+
+# ##############################
+
+# # a= int(input("Enter Number for you want table: "))
+# # int a= 0;
+# # for(int i )
+
+# # number = int(input(""))
+# # count = 1
+# # while count <= 10:
+# #     number = number * 1
+# #     print(number, 'x', count, '=', number * count)
+# #     count += 1
+
+
+# def factorial1(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return n* factorial1(n-1)
+#         n= int(input("input a number to compute the factorial : "))
+#         print(factorial1(n))
+def (fn)
